3fe95b95f373772200101c1bce6a89fc86e987a9
Juandhm18 <juandhm20@gmail.com>
2025-10-28 22:59:16 -0500

    feat: HU-0 Modelo Users con su logica basica creada

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,7 +1,27 @@
-export class User{
-    id: number;
-    name: string;
-    email: string;
-    password: string;
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+import { Task } from 'src/tasks/entities/task.entity';
+import { Exclude } from 'class-transformer';
+
+@Entity()
+export class User {
     
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  @Exclude()
+  password_hash: string;
+
+  @OneToMany(() => Task, (task) => task.user)
+  tasks: Task[];
+
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import { CreateUserDto } from '../tasks/dto/create-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  create(dto: CreateUserDto): Promise<User> {
+    const user = this.userRepository.create(dto);
+    return this.userRepository.save(user);
+  }
+
+  findByEmail(email: string): Promise<User | null> {
+    return this.userRepository.findOne({ where: { email } });
+  }
+
+  findById(id: number): Promise<User | null> {
+    return this.userRepository.findOne({ where: { id } });
+  }
+}
